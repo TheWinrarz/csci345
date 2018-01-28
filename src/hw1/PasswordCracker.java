cb7bd61d7a52d894d43903514ac3c1f1b6416460
DanielAustinLee <danielaustinlee@gmail.com>
2018-01-27 20:44:42 -0500

    readInput() function

---
+++ src/hw1/PasswordCracker.java
@@ -0,0 +1,31 @@
+package hw1;
+
+import java.io.File;
+
+//USE DigestUtils first
+import java.security.MessageDigest;
+
+
+
+class PasswordCracker {
+
+    static File passwordFile;
+
+
+    //static MessageDigest md = MessageDigest.getInstance();
+
+    public static void readInput(String filename){
+
+        passwordFile = new File(filename);
+
+
+    }
+
+
+    public static void hash(String preimage){
+
+    }
+
+
+
+}

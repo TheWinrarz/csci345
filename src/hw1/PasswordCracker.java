9ab8380c7feb226b2efd85f7044b2566df2364ff
DanielAustinLee <danielaustinlee@gmail.com>
2018-01-28 20:37:33 -0500

    Create hash function

--- src/hw1/PasswordCracker.java
+++ src/hw1/PasswordCracker.java
@@ -3,18 +3,26 @@ package hw1;
 import java.io.File;
 
 //USE DigestUtils first
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
-
+import java.security.NoSuchAlgorithmException;
 
 
 class PasswordCracker {
 
     static File passwordFile;
 
+    static MessageDigest md;
 
-    //static MessageDigest md = MessageDigest.getInstance();
+    public PasswordCracker(){
 
-    public static void readInput(String filename){
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {}
+    }
+
+    private static void readInput(String filename){
 
         passwordFile = new File(filename);
 
@@ -22,10 +30,36 @@ class PasswordCracker {
     }
 
 
-    public static void hash(String preimage){
+    public static String hash(String preimage){
 
+        String hashtext = "";
+
+        try {
+
+            byte[] bytesOfPreimage = preimage.getBytes("US-ASCII");
+            byte[] digest = md.digest(bytesOfPreimage);
+
+            BigInteger bigInt = new BigInteger(1,digest);
+            hashtext = bigInt.toString(16);
+
+            while(hashtext.length() < 32 ){
+                hashtext = "0"+hashtext;
+            }
+
+
+        } catch (UnsupportedEncodingException e) {}
+
+
+        return hashtext;
     }
 
+    public static void main(String[] args){
+        PasswordCracker c = new PasswordCracker();
+        System.out.println(hash("1234"));
+        System.out.println(hash("Hair6"));
+        System.out.println(hash("b3ach"));
+        System.out.println(hash("9wXy!"));
+    }
 
 
 }
